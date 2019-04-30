f3f40139e3d8ed9f375b988246f7948ea47502d8
James Gomez <james.gomez320@yahoo.com>
2019-04-30 01:12:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
+<<<<<<< HEAD
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
     "FizzBuzz"
@@ -18,3 +19,20 @@ def fizzbuzz(int)
   
 end
 end
+=======
+def fizzbuzz()
+  if fizz_3=fizzbuzz(3)
+    puts "Fizz"
+  
+  elsif fizz_5=5
+    puts "buzz"
+    
+  elsif fizz_15==15
+    puts "FizzBuzz"
+  
+else puts "nil"
+  
+  
+end
+end
+>>>>>>> 4db425b08eebafe8f84534b661ee25f8842cd8fe
